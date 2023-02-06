50e0f2b9f06f43752dfabccf000446e8c0d8faa6
Jalopezm <jalopezm@esliceu.net>
2023-02-06 18:34:47 +0100

    Error 404 added to ErrorPage.js

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -2,15 +2,26 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
-  return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+  if (error.status === 404) {
+    return (
+      <div>
+        <h1>404</h1>
+        <p>Oh the irony of developing software which helps find and fix broken links, 
+          and then having some yourself! This was just a test, honest. </p>
+        <p> Or perhaps you entered an incorrect URI? Contact us, if you need any help at all.</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    );
+  }
+
 }
