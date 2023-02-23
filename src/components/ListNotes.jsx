2295b07244e04da4fc52ca795784cba7b30fe5be
Jalopezm <jalopezm@esliceu.net>
2023-02-23 17:04:19 +0100

    Missing Change Password

--- src/components/ListNotes.jsx
+++ src/components/ListNotes.jsx
@@ -6,8 +6,13 @@ export default function ListNotes() {
   let navigate = useNavigate();
   const token = localStorage.getItem("jwt");
   const [notes, setNotes] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+  const [findBody, setFindBody] = useState("");
+  const [findTitle, setFindTitle] = useState("");
+  const [findType, setFindType] = useState("");
 
   useEffect(() => {
+    let setTimes = [];
     async function getNote() {
       await fetch("http://localhost:8081/notes", {
         method: "GET",
@@ -20,13 +25,59 @@ export default function ListNotes() {
           return response.json();
         })
         .then((data) => {
-          setNotes(data);
+          console.log("ARRAY" + data);
+          let filterByTitle = data.filter((note) =>
+            note.title.toUpperCase().includes(findTitle.toUpperCase())
+          );
+          console.log(filterByTitle);
+          let filterByBody = filterByTitle.filter((note) =>
+            note.body.toUpperCase().includes(findBody.toUpperCase())
+          );
+          console.log(filterByBody);
+
+          let filterByType = filterByBody.filter((note) => {
+            if (findType !== "") {
+              return findType !== "Audio"
+                ? !note.isVoiceNote
+                : note.isVoiceNote;
+            } else {
+              return filterByBody;
+            }
+          });
+          for (let i = 0; i < filterByType.length; i++) {
+            setTimes.push(formatTime(filterByType[i]));
+          }
+
+          setNotes(sortNotesBy(filterByType, sortBy));
         });
     }
 
     getNote();
-  }, [token]);
+  }, [token, findBody, findTitle, findType, sortBy]);
 
+  const formatTime = (data) => {
+    data.createdAt = new Date(data.createdAt).getTime()
+    data.modifiedAt = new Date(data.modifiedAt).getTime()
+  };
+
+  const sortNotesBy = (data, key) => {
+    switch (key) {
+      case "title":
+        data.sort(function (a, b) {
+          let titleA = a.title.toUpperCase();
+          let titleB = b.title.toUpperCase();
+          return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+        });
+        break;
+
+      default:
+        data.sort(function (a, b) {
+          return a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0;
+        });
+        break;
+    }
+    return data;
+  };
   async function deleteNote(id) {
     const response = await fetch(`http://localhost:8081/notes/${id}`, {
       method: "DELETE",
@@ -42,14 +93,48 @@ export default function ListNotes() {
 
   return (
     <>
+      <label htmlFor="sortBy">Sort by: </label>
+      <select
+        name="sortBy"
+        id="select_sortBy"
+        onChange={(e) => setSortBy(e.target.value)}
+      >
+        <option value="">Sort By</option>
+        <option value="title">Title</option>
+        <option value="createdAt">Creation Date</option>
+        <option value="modifiedAt">Last Update</option>
+      </select>
+
+      <label htmlFor="findType">Find By Type:</label>
+      <select
+        name="findType"
+        id="select_type"
+        onChange={(e) => setFindType(e.target.value)}
+      >
+        <option value="">Find By Type</option>
+        <option value="Text">Text</option>
+        <option value="Audio">Audio</option>
+      </select>
+
+      <label htmlFor="findTitle">Find By Title:</label>
+      <input
+        type="text"
+        name="findTitle"
+        onChange={(e) => setFindTitle(e.target.value)}
+      />
+
+      <label htmlFor="findBody">Find By Content:</label>
+      <input
+        type="text"
+        name="findBody"
+        onChange={(e) => setFindBody(e.target.value)}
+      />
       <table className="note_table">
         <thead>
           <tr>
             <th className="table_header">UserId</th>
             <th className="table_header">Title</th>
-            <th className="table_header">Created At Date</th>
             <th className="table_header">Created At Hour</th>
-            <th className="table_header">Modified At Date</th>
             <th className="table_header">Modified At Hour</th>
             <th className="table_header">View Note</th>
             <th className="table_header">Delete</th>
@@ -61,11 +146,9 @@ export default function ListNotes() {
             <tr key={index}>
               <td className="table_data">{note.userId}</td>
               <td className="table_data">{note.title}</td>
-              <td className="table_data">{note.createdAt.slice(0, 10)}</td>
               <td className="table_data">
                 {new Date(note.createdAt).toLocaleTimeString()}
               </td>
-              <td className="table_data">{note.modifiedAt.slice(0, 10)}</td>
               <td className="table_data">
                 {new Date(note.modifiedAt).toLocaleTimeString()}
               </td>
@@ -78,7 +161,9 @@ export default function ListNotes() {
                 <button onClick={(e) => deleteNote(note.id)}>Delete</button>
               </td>
               <td className="table_update">
-                <button onClick={(e) => navigate("/notes/" + note.id+"/edit")}>
+                <button
+                  onClick={(e) => navigate("/notes/" + note.id + "/edit")}
+                >
                   Update
                 </button>
               </td>
